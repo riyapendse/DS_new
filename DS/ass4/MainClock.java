a34e811bf0577b76b35a5e5a5aba06df8bdb9efe
Riya Pendse <89235892+riyapendse@users.noreply.github.com>
2024-04-24 04:18:51 +0530

    Add files via upload

---
+++ DS/ass4/MainClock.java
@@ -0,0 +1,61 @@
+
+import java.rmi.Naming;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class MainClock {
+    public static void main(String args[]){
+        try {
+            String clock1 = "rmi://" + "127.0.0.1/" + args[0];
+        String clock2 = "rmi://" + "127.0.0.1/" + args[1];
+        String clock3 = "rmi://" + "127.0.0.1/" + args[2];
+
+        LocalTime serverTime = LocalTime.parse("03:00:00",DateTimeFormatter.ofPattern("HH:mm:ss"));
+        System.out.println("Server Time is "+serverTime);
+
+        ServerIntf c1 = (ServerIntf) Naming.lookup(clock1);
+        System.out.println(c1.getTime());
+
+        ServerIntf c2 = (ServerIntf) Naming.lookup(clock2);
+        System.out.println(c2.getTime());
+
+        ServerIntf c3 = (ServerIntf) Naming.lookup(clock3);
+        System.out.println(c3.getTime());
+
+        
+        long serverNano = serverTime.toNanoOfDay();
+        long c1Nano = c1.getTime().toNanoOfDay()-serverNano;
+        long c2Nano = c1.getTime().toNanoOfDay()-serverNano;
+        long c3Nano = c1.getTime().toNanoOfDay()-serverNano;
+        long avg = (c1Nano+c2Nano+c3Nano)/4;
+
+        c1.adjustTime(serverTime, avg);
+        c2.adjustTime(serverTime, avg);
+        c3.adjustTime(serverTime, avg);
+        serverTime.plusNanos(avg);
+
+        System.out.println("updated time"+serverTime);
+        } catch (Exception ex) {
+            System.out.println("Exception at main "+ex);
+        }
+    }
+}
+
+// terminal 1:
+// javac *.java
+// rmic AddServerImpl
+// rmiregistry
+
+// terminal 2:
+// java C1 c1 03:25:00
+
+// terminal 3:
+// java C1 c2 03:00:00
+
+// terminal 4:
+// java C1 c3 02:55:00
+
+// terminal 5:
+// java MainClock c1 c2 c3
+
